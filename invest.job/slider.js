9cfc902fa3685e1eb70cf2941849c30d234bf44e
bekzatzrl <167733580+bekzatzrl@users.noreply.github.com>
2024-04-22 10:19:49 +0600

    Add files via upload

---
+++ invest.job/slider.js
@@ -0,0 +1,21 @@
+$(".slider").owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000, //2000ms = 2s;
+    autoplayHoverPause: true,
+  });
+  // var swiper = new Swiper(".mySwiper", {
+  //   slidesPerView: 3,
+  //   spaceBetween: 30,
+  //   slidesPerGroup: 3,
+  //   loop: true,
+  //   loopFillGroupWithBlank: true,
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //   },
+  //   navigation: {
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev",
+  //   },
+  // });
